3e6023b8459131dbb97f3e1f6a48bd3c39d805e4
DatPTPD11090 <Phantuandat91203@gmail.com>
2024-03-25 18:57:46 +0700

    Assignment GD1

---
+++ Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Circle"))
+        {
+            checkpoint();
+            
+        }    
+    }
+    private void checkpoint()
+    {
+
+    }
+    
+        
+    
+}
