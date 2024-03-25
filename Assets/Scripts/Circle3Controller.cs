784331aa8ecd02e46e45eabdd630a6a3606aab81
DatPTPD11090 <Phantuandat91203@gmail.com>
2024-03-25 11:30:19 +0700

    Up code Lab4

---
+++ Assets/Scripts/Circle3Controller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Circle3downController : MonoBehaviour
+{
+    private int direction = 1;
+    private float moveSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(0f,direction,0f);
+        transform.Translate(movement*moveSpeed *Time.deltaTime);
+        if(transform.position.y > 2.02f || transform.position.y < -2.19f)
+        {
+            direction *= -1;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Square3Controller.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Square3Controller : MonoBehaviour
+{
+    public float timeReaining = 60;
+    public Text countdownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+    IEnumerator Countdown()
+    {
+        while (timeReaining > 0)
+        {
+            yield return new WaitForSeconds(1);
+            timeReaining--;
+            countdownText.text = "Time: " + timeReaining.ToString();
+        }
+        countdownText.text = "Time's up!";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, vertical, 0f).normalized;
+        transform.Translate(movement * 2f * Time.deltaTime);
+        
+    }
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Circle"))
+        {
+            Debug.Log("YOU DIE!!!");
+            Vector2 fistPosition = new Vector2(-4.81f, 3.59f);
+            transform.position = fistPosition;
+        }
+        if (collision.gameObject.name.Equals("Box"))
+        {
+            Debug.Log("YOU WIN!!!");
+            LoadNextScene();
+        }
+        if (collision.gameObject.tag.Equals("X"))
+        {
+            Vector2 fistPosition = new Vector2(-4.81f, 3.59f);
+            transform.position = fistPosition;
+        }
+    }
+}
